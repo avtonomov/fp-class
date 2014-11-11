19db1cf48313c6dd805703a8618b829574ba387c
Dima <avtonomovvv@gmail.com>
2014-11-11 20:42:34 +0400

    task1

--- class-09/01-totalLength.hs
+++ class-09/01-totalLength.hs
@@ -1,11 +1,14 @@
 import System.Environment
-
+import System.Random
+import System.IO
+import System.Directory
+import Data.Char
 {-
   Написать функцию, которая по заданному списку строк возвращает сумму длин всех строк.
 -}
 
 totalLength :: [String] -> Int
-totalLength = undefined
+totalLength (xs) = foldl (\acc x ->  acc + length x) 0 (xs)
 
 {-
   Написать функцию, которая по заданному символу и целому числу n строит список строк,
@@ -13,7 +16,11 @@ totalLength = undefined
 -}
 
 build1 :: Char -> Int -> Maybe [String]
-build1 = undefined
+build1 ch 0 = Nothing
+build1 ch n = Just([replicate acc ch | acc <- [1..n]])
+
+ 
+
 
 {-
   Написать функцию, аналогичную по возможностям функции build1, но возвращающую при этом
@@ -25,7 +32,11 @@ build1 = undefined
 -}
 
 build2 :: Char -> Int -> Either String [String]
-build2 = undefined
+build2 ch n
+     |n == 0 = Left ("n=0")
+     |n >100 = Left ("n>0")
+     |ch =='x' = Left ("Роспотребнадзор запрещает создавать строки из символа 'x'")
+     |otherwise = Right([replicate acc ch | acc <- [1..n]])
 
 {-
   Параметрами командной строки являются имя файла, символ, целое число.
@@ -38,6 +49,23 @@ build2 = undefined
      сформированных функциями build1 и build2 (в решении следует пользоваться возможностями
      Maybe и Either String как функторов).
 -}
+{-
+[file, ch,n] <- getArgs
+  handle <- openFile file ReadMode
+  contents <- hGetContents handle
+  print (totalLength (lines contents))
+  hClose handle
+-}
+
 
 main = do
-  undefined
+  [file, ch,n] <- getArgs
+  let num = (read n :: Int)
+  task1 <- fmap totalLength getArgs
+  task2 <- fmap (totalLength.lines) (readFile file)
+  let task3 = fmap (totalLength) (build1 (head ch) num)
+  let task31 = fmap (totalLength) (build2 (head ch) num)
+  print task1
+  print task2
+  print task3
+  print task31
