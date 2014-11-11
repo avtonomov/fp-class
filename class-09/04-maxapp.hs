12b38b114cd9c3248a81bc76061058e578ba7944
Dima <avtonomovvv@gmail.com>
2014-11-11 20:44:02 +0400

    task4

--- class-09/04-maxapp.hs
+++ class-09/04-maxapp.hs
@@ -8,24 +8,28 @@ import Control.Applicative
 -}
 
 maxApp2 :: (Ord a, Applicative f) => f a -> f a -> f a
-maxApp2 = undefined
+maxApp2 a b= max <$> a <*> b 
 
 {- Реализуйте аналогичную функцию в случае трёх заданных значений в контексте. -}
 
 maxApp3 :: (Ord a, Applicative f) => f a -> f a -> f a -> f a
-maxApp3 = undefined
+maxApp3 a b c= max <$> a <*> (max <$> a <*> b) 
 
 {- Реализуйте аналогичную функцию в случае списка значений в контексте. -}
 
 maxApp :: (Ord a, Applicative f) => [f a] -> f a
-maxApp = undefined
+maxApp = foldl1 maxApp2
 
+ 
 {-
   Продемонстрируйте использование написанных функций для аппликативных функторов Maybe,
   список (для каждого из двух экземпляров), Either String и IO.
 -}
 
-main = undefined
+main = do 
+	print $ maxApp2 (Just 5) (Just 3)
+	print $ maxApp3 (Just 5) (Just 3) (Just 9)
+	print $ maxApp [Just 5,Just 3]
 
 {- (необязательно)
   Ясно ли вам, что вы реализовали нечто, похожее на моноид на аппликативных функторах?
