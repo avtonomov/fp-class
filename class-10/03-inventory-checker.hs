5d3012833607be591a9e7b818f48d488b3fe2b67
Dima <avtonomovvv@gmail.com>
2014-11-18 20:52:27 +0400

    task3

--- class-10/03-inventory-checker.hs
+++ class-10/03-inventory-checker.hs
@@ -1,6 +1,7 @@
 import Control.Monad
 import Data.List
 import System.Environment
+import Data.Maybe
 
 {-
    Дан текстовый файл (inventory.txt)  с перечислением всей имеющейся на складе
@@ -18,13 +19,23 @@ data ArmorItem = ArmorItem ArmorKind ArmorType
 data ArmorKit = ArmorKit ArmorKind [ArmorType]
    deriving (Show, Eq)
 
+kinds = [Chitin, Hide, Leather, Elven, Scaled, Glass, ImperialLight]
+read_item [armor_kind, armor_type] = ArmorItem (read armor_kind) (read armor_type)
+
 loadInventory :: FilePath -> IO [ArmorItem]
-loadInventory = undefined
+loadInventory fname = readFile fname >>= return . map (read_item . words) . lines
+
+		
+
 
 buildArmorKit :: ArmorKind -> [ArmorItem] -> Maybe ArmorKit
-buildArmorKit = undefined
+buildArmorKit kind xs
+    | length temp == 5 = Just (ArmorKit kind temp)
+	| otherwise = Nothing
+	where
+		temp = foldl (\acc (ArmorItem kind_ x) -> if kind_ == kind then x:acc else acc ) [] xs
 
 buildKits :: [ArmorItem] -> Maybe [ArmorKit]
-buildKits = undefined
+buildKits armor_items = sequence $ filter (isJust) $ zipWith buildArmorKit kinds (replicate 7 armor_items)
 
-main = (head `liftM` getArgs) >>= loadInventory >>= undefined >>= print
+main = (head `liftM` getArgs) >>= loadInventory  >>= print.buildKits
