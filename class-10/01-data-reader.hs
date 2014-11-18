1b3c291bba04ea603e3a0ddf934f68757c6902b9
Dima <avtonomovvv@gmail.com>
2014-11-18 20:51:25 +0400

    task1

--- class-10/01-data-reader.hs
+++ class-10/01-data-reader.hs
@@ -9,4 +9,23 @@
    блока do не допускается).
 -}
 
-main = undefined
+
+import System.Environment
+import Control.Monad
+import Data.List
+import Data.Ord
+
+
+data Students = Students {name::String, age::Int, number_group::String}
+                                               deriving (Show,Ord,Eq)
+
+read_to_student_array [] = []
+read_to_student_array (n:a:number:xs) = [Students n (read a) number]:read_to_student_array (xs)
+
+
+read_from_file fname = readFile fname >>= return . read_to_student_array . lines
+
+write_to_file fname xs = writeFile fname $ unlines $ map show xs
+
+union_lists  fname1 fname2 = (++) `liftM` (read_from_file fname1) `ap` (read_from_file fname2)  >>= write_to_file "new_list.txt" .sort
+main =  union_lists "list1.txt" "list2.txt"
